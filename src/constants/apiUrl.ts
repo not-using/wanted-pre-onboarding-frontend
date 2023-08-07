459eda3303b530b4db3c4e88ab7567759f3ba68a
sujikim <sujikim@student.42seoul.kr>
2023-08-08 04:43:27 +0900

    feat: 서버와의 통신을 위한 기본 axios 설정
    - api 기본 주소 추가
    - axios 인스턴스 및 useApi 훅 추

---
+++ src/constants/apiUrl.ts
@@ -0,0 +1,2 @@
+// 과제 제약사항상 .env를 사용하지 못하기 때문에 하드코딩
+export const apiUrl = "https://www.pre-onboarding-selection-task.shop";
---
+++ src/constants/axios.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { apiUrl } from "constants/apiUrl";
+
+export const axiosInstance = axios.create({
+	baseURL: apiUrl,
+});
