cf178f20065236bc8e8fdd42f8b525e450d99608
sujikim <sujikim@student.42seoul.kr>
2023-08-07 18:20:10 +0900

    feat: A1 로그인/회원가입 유효성 검사 구현

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,13 +4,24 @@ import { styled } from "styled-components";
 interface IProps extends ComponentProps<"input"> {
 	id: string;
 	labelText: string;
+	isValid?: boolean;
+	invalidMessage?: string;
 }
 
-const Input = ({ id, labelText, ...rest }: IProps) => {
+const Input = ({
+	id,
+	labelText,
+	value,
+	isValid = true,
+	invalidMessage = "",
+	...rest
+}: IProps) => {
+	const isEmpty = value === "" || undefined;
 	return (
 		<Wrapper>
 			<label htmlFor={id}>{labelText}</label>
-			<CustomInput id={id} {...rest} />
+			<CustomInput id={id} {...rest} value={value} />
+			<ErrorText>{isEmpty || isValid ? "" : invalidMessage}</ErrorText>
 		</Wrapper>
 	);
 };
@@ -21,5 +32,10 @@ const Wrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 `;
-const CustomInput = styled.input`
+const CustomInput = styled.input``;
+
+const ErrorText = styled.span`
+	color: red;
+	font-size: 0.8rem;
+	height: 1rem;
 `;
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { authPolicy } from "constants/authPolicy";
 import Button from "components/Button";
 import Input from "components/Input";
-import { useLocation } from "react-router-dom";
 
 const Auth = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	const isSignup = useLocation().pathname === "/signup";
 	const stage = isSignup ? "회원가입" : "로그인";
+	const isEmailValid = authPolicy.email.validator(email);
+	const isPasswordValid = authPolicy.password.validator(password);
 
 	return (
 		<main>
@@ -13,18 +20,26 @@ const Auth = () => {
 				<Input
 					id="email"
 					labelText="이메일"
-					type="email"
 					data-testid="email-input"
+					value={email}
+					isValid={isEmailValid}
+					invalidMessage={authPolicy.email.invalidMessage}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<Input
 					id="password"
 					labelText="비밀번호"
 					type="password"
 					data-testid="password-input"
+					value={password}
+					isValid={isPasswordValid}
+					invalidMessage={authPolicy.password.invalidMessage}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 				<Button
 					type="submit"
 					data-testid={`sign${isSignup ? "up" : "in"}-button`}
+					disabled={isEmailValid && isPasswordValid ? false : true}
 				>
 					{stage}
 				</Button>
