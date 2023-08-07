65f5592dd9af62da54ebac4f585d6aa31545bdbc
sujikim <sujikim@student.42seoul.kr>
2023-08-07 19:49:47 +0900

    fix: 테스트 설정을 위해 router에서 routes 분리

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -8,13 +8,13 @@ import Main from "pages/Main";
 import NotFound from "pages/NotFound";
 import Auth from "pages/Auth";
 
-export const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<Layout />}>
-			<Route index element={<Main />} />
-			<Route path="signup" element={<Auth />} />
-			<Route path="signin" element={<Auth />} />
-			<Route path="*" element={<NotFound />} />
-		</Route>
-	)
+export const routes = createRoutesFromElements(
+	<Route path="/" element={<Layout />}>
+		<Route index element={<Main />} />
+		<Route path="signup" element={<Auth />} />
+		<Route path="signin" element={<Auth />} />
+		<Route path="*" element={<NotFound />} />
+	</Route>
 );
+
+export const router = createBrowserRouter(routes);
