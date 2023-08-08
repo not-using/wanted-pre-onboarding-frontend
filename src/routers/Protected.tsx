73002e1bb779ee98b675238b9624ce4499c7835a
sujikim <sujikim@student.42seoul.kr>
2023-08-08 17:05:21 +0900

    feat: A4 로그인 및 인증 여부에 따른 리다이렉트 처리 구현

---
+++ src/routers/Protected.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+interface IProps {
+	authenticated: "forbidden" | "allowed";
+	replace: string;
+}
+
+const Protected = ({ authenticated, replace }: IProps) => {
+	const token = localStorage.getItem("token");
+	const isAllowedRoute = authenticated === "forbidden" ? !token : !!token;
+
+	return isAllowedRoute ? <Outlet /> : <Navigate to={replace} />;
+};
+
+export default Protected;
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -7,12 +7,21 @@ import Layout from "pages";
 import Main from "pages/Main";
 import NotFound from "pages/NotFound";
 import Auth from "pages/Auth";
+import Todo from "pages/Todo";
+import Protected from "routers/Protected";
 
 export const routes = createRoutesFromElements(
 	<Route path="/" element={<Layout />}>
 		<Route index element={<Main />} />
-		<Route path="signup" element={<Auth />} />
-		<Route path="signin" element={<Auth />} />
+		{/* 토큰 인증을 받지 않은 사용자만 접근 가능 */}
+		<Route element={<Protected authenticated="forbidden" replace="/todo" />}>
+			<Route path="signup" element={<Auth />} />
+			<Route path="signin" element={<Auth />} />
+		</Route>
+		{/* 토큰 인증을 받은 사용자만 접근 가능 */}
+		<Route element={<Protected authenticated="allowed" replace="/signin" />}>
+			<Route path="todo" element={<Todo />} />
+		</Route>
 		<Route path="*" element={<NotFound />} />
 	</Route>
 );
