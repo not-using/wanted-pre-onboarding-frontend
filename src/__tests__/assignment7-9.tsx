d1fae538c9dca8ff7be916c6ee8eff9945f3d7ec
sujikim <sujikim@student.42seoul.kr>
2023-08-11 00:47:13 +0900

    test: A8, A9 수정 버튼과 삭제 버튼, 삭제 기능 테스트 추가

--- src/__tests__/assignment7-9.tsx
+++ src/__tests__/assignment7-9.tsx
@@ -28,7 +28,7 @@ test("투두의 체크박스를 통해 완료 여부를 수정할 수 있다", a
 	});
 
 	act(() => {
-		user.click(screen.getByRole("checkbox", { name: "todo test 1" }));
+		user.click(screen.getByRole("checkbox", { name: "todo test 1 수정 삭제" }));
 	});
 
 	// 미완료 투두 1개로 변화
@@ -38,12 +38,45 @@ test("투두의 체크박스를 통해 완료 여부를 수정할 수 있다", a
 	});
 
 	act(() => {
-		user.click(screen.getByRole("checkbox", { name: "todo test 1" }));
+		user.click(screen.getByRole("checkbox", { name: "todo test 1 수정 삭제" }));
 	});
 
 	// 미완료 투두 2개로 다시 변화
 	await waitFor(() => {
 		const checkboxes: HTMLInputElement[] = screen.getAllByRole("checkbox");
 		expect(checkboxes.filter((box) => box?.checked === false)).toHaveLength(2);
+	});
+});
+
+test("투두 우측에 수정버튼과 삭제버튼을 확인할 수 있다", async () => {
+	jest.spyOn(Storage.prototype, "getItem").mockImplementation(() => "ok");
+	nock(apiUrl).get("/todos").reply(200, todos);
+
+	setup(["/todo"]);
+
+	await waitFor(() => {
+		expect(screen.getAllByTestId("modify-button")).toHaveLength(3);
+		expect(screen.getAllByTestId("delete-button")).toHaveLength(3);
+	});
+});
+
+test("투두 삭제 버튼을 클릭하면 해당 아이템이 삭제된다", async () => {
+	jest.spyOn(Storage.prototype, "getItem").mockImplementation(() => "ok");
+	nock(apiUrl).get("/todos").reply(200, todos);
+
+	setup(["/todo"]);
+
+	// 삭제 전 투두 3개 확인
+	await waitFor(() => {
+		expect(screen.getAllByRole("listitem")).toHaveLength(3);
+	});
+
+	act(() => {
+		user.click(screen.getAllByRole("button", { name: "삭제" })[0]);
+	});
+
+	// 삭제 후 투두 2개로 변화
+	await waitFor(() => {
+		expect(screen.getAllByRole("listitem")).toHaveLength(3);
 	});
 });
