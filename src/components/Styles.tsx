dfaf4b68cc7ce2ddad6dd8c4c39272ad48a49eb0
sujikim <sujikim@student.42seoul.kr>
2023-08-12 19:13:41 +0900

    feat: 메인페이지 환영 문구 및 /todo 링크 추가

--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -5,3 +5,10 @@ export const StyledLink = styled(Link)`
 	text-decoration: none;
 	color: black;
 `;
+
+export const FlexColumn = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
