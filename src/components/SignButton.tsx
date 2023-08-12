6496b07d8c0e0672de4e0699ce87d895c2205a55
sujikim <sujikim@student.42seoul.kr>
2023-08-12 18:45:07 +0900

    feat: 토큰 상태에 따라 달라지는 로그인/로그아웃 버튼 추가
    url가 변경될때마다 토큰을 확인하도록 함

---
+++ src/components/SignButton.tsx
@@ -0,0 +1,57 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { StyledLink } from "components/Styles";
+import Button from "components/Button";
+
+const SignButton = () => {
+	const [isSignedIn, setIsSignedIn] = useState(false);
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setIsSignedIn(!!localStorage.getItem("token"));
+	}, [location]);
+
+	const singout = () => {
+		localStorage.removeItem("token");
+		navigate("/");
+	};
+
+	return isSignedIn ? (
+		<SignOutButton onClick={singout}>로그아웃</SignOutButton>
+	) : (
+		<span>
+			<SignUpLink to="/signup">
+				회원이 아니신가요? <Bold>회원가입</Bold>
+			</SignUpLink>
+			<SignInLink to="/signin">로그인</SignInLink>
+		</span>
+	);
+};
+
+export default SignButton;
+
+const SignUpLink = styled(StyledLink)`
+	color: grey;
+`;
+
+const Bold = styled.b`
+	text-decoration: underline;
+	color: black;
+`;
+
+const SignInLink = styled(StyledLink)`
+	margin-left: 1rem;
+	padding: 0.25rem 1rem;
+	background-color: black;
+	color: white;
+	border-radius: 0.25rem;
+`;
+
+const SignOutButton = styled(Button)`
+	padding: 0.5rem 1rem;
+	background-color: black;
+	color: white;
+	border-radius: 0.25rem;
+`;
